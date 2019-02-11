5315ebea3b4c4863b75f118d88890bc0efee3a6e
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-12 01:41:26 +0900

    グループの編集機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,21 +12,40 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      # group_idにマッチするpathもインスタンスから推定
-      redirect_to group_messages_path(@group), notice: "グループを作成しました。"
+      redirect_to url_after_create_or_update, notice: "グループを作成しました。"
     else
       render :new
     end
   end
 
   def edit
+    set_group
   end
 
   def update
+    set_group
+    if @group.update(group_params)
+      redirect_to url_after_create_or_update, notice: "グループを変更しました。"
+    else
+      render :edit
+    end
   end
 
   protected
   def group_params
     params.require(:group).permit(:name, {user_ids: []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def url_after_create_or_update
+    if @group.users.exists?(current_user.id)
+      # group_idにマッチするpathもインスタンスから推定可能
+      group_messages_url(@group)
+    else
+      root_url
+    end
   end
 end
