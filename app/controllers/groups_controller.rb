07462e234b6e2a3cdec5474d8d8f4d7602b9b638
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-11 20:23:09 +0900

    group#createの失敗時のエラーメッセージ表示を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,11 +7,22 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_messages_path(@group)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  protected
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
