d3005f0f4a30fe1d3498456cec7852eba00a6a79
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-11 18:35:12 +0900

    group, messageのコントローラ、ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
+  # root to: "messages#index"
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # resources :messages, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
