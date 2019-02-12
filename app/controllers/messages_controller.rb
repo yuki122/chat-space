7fc428ed245e29570860347716ff9ac1e54a24dd
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-12 11:21:07 +0900

    投稿機能を実装（renderでcssが効かなくなる問題あり）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,37 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:create, :index]
+
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    # TODO: N+1問題について要検討
-    @messages = @group.messages.order("created_at")
+    set_messages
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.group = @group
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
+    else
+      # renderのためにインスタンス変数を用意する必要
+      # @groups = current_user.groups
+      # set_messages
+      # flash.now[:alert] = "メッセージを入力して下さい"
+      # render :index
+
+      # flexboxがrenderだとなぜか効かないので、一旦redirectに
+      redirect_to group_messages_path(@group), alert: "メッセージを入力して下さい"
+    end
+  end
+
+  protected
+  def message_params
+    params.require(:message).permit([:body, :image]).merge(user_id: current_user.id)
+  end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+  def set_messages
+    @messages = @group.messages.includes(:user).order("created_at")
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,8 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
+  # TODO: この書き方でいいのか
+  validates :body, presence: true, if: "image.blank?"
+
   mount_uploader :image_path, MessageImageUploader
 end
