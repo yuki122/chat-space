3855d50c8a91067aec2f0fa31f40c8f04576fcc5
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-15 22:02:01 +0900

    ajaxでmessages#createにpost（controllerの処理は未実装）

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,6 +1,20 @@
 $(function(){
   $(".message-form").on("submit", function(e){
-    console.log("submit");
+    const formData = new FormData(this);
+    const url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    }).done(function(data){
+      $(".message-form__field__body").val("");
+    }).fail(function(data){
+      console.log(data);
+      alert("error");
+    });
 
     // これがないと連続でjsが実行されない（preventDefault()ではダメだった）
     return false;
