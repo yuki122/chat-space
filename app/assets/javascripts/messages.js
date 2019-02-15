d834426c1e6795ca461b20600e7784a17032f8ab
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-15 21:52:30 +0900

    フォーム送信イベントを取得

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,3 +1,8 @@
 $(function(){
-  console.log("js worked");
+  $(".message-form").on("submit", function(e){
+    console.log("submit");
+
+    // これがないと連続でjsが実行されない（preventDefault()ではダメだった）
+    return false;
+  });
 });
