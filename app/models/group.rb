a74cf68ce441d896bdb711a985d1adf277c7cfbe
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-18 21:05:46 +0900

    group.nameのuniqueバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   has_many :messages
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def show_last_message
     if(last_message = self.messages.order("created_at").last)
