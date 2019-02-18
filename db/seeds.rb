d39c939157a891e08952a031587ee56f97b8dfd2
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-18 17:39:35 +0900

    テスト用にseedでユーザを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |i|
+  password = "password"
+  User.create!(
+    # name: Faker::Name.name,
+    name: Faker::Internet.user_name,
+    email: "sample-#{i+1}@example.com",
+    password: password,
+    password_confirmation: password)
+end
