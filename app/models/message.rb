c805cf60eb8bfe850cfc416e43c4ac2620db71e4
横山勇輝 <simnykymyu@yahoo.co.jp>
2019-02-18 23:54:08 +0900

    Time型とStrringの変換をmessageモデルで定義

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,19 @@
 class Message < ApplicationRecord
+  TIME_FORMAT = '%Y年%m月%d日 %H:%M:%S'
+
   belongs_to :group
   belongs_to :user
 
   validates :body, presence: true, if: "image.blank?"
 
   mount_uploader :image, MessageImageUploader
+
+  def created_at_for_disp
+    self.created_at.strftime(TIME_FORMAT)
+  end
+
+
+  def self.parse_str_to_time(str_time)
+    Time.strptime(str_time, TIME_FORMAT)
+  end
 end
